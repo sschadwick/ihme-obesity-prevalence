af43d35f5710020731607e63d1c819c78ac879fd
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-02-08 12:30:59 -0800

    fixed index.html

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var fs = require('fs');
 var fstream = require('fstream');
 var url = require('url');
 
+app.use(express.static('public'));
+
 var readStream = fs.createReadStream('public/IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.zip');
 var writeStream = fstream.Writer('public/');
 
 readStream
   .pipe(unzip.Parse())
   .pipe(writeStream);
-
-app.use(express.static('public'));
 
 app.use(function(req, res) {
   res.status(404).send('Page not found');
