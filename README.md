d45277c5ee8c03f8ee074de27f79668f0b9800a7
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-02-08 12:19:44 -0800

    added unzip stream

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ihme-obesity-prevalence
+# D3 Visualization of Global Obesity Ddata from IHME.
+
+Global Burden of Disease Study 2013. Global Burden of Disease Study 2013 (GBD 2013) Obesity Prevalence 1990-2013. Seattle, United States: Institute for Health Metrics and Evaluation (IHME), 2014. 
--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 
 var express = require('express');
 var app = express();
+var unzip = require('unzip');
+var fs = require('fs');
+var fstream = require('fstream');
+var url = require('url');
+
+var readStream = fs.createReadStream('public/IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.zip');
+var writeStream = fstream.Writer('public/');
+
+readStream
+  .pipe(unzip.Parse())
+  .pipe(writeStream);
 
 app.use(express.static('public'));
 
